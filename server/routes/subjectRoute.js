075d97c91969f0be67a1248136adcb9555c8fd67
Vivin Jeganathan <VivinJeganathan@yahoo.com>
2018-09-11 21:35:29 -0500

    Subject patching donr

--- server/routes/subjectRoute.js
+++ server/routes/subjectRoute.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const ObjectID = require('mongodb').ObjectID
 
+const _ = require('lodash')
 var { Subject } = require('../models/subject');
 
 app.get('/', (request, response) => {
@@ -15,55 +16,62 @@ app.get('/', (request, response) => {
     })
 });
 
-app.patch('/subject', (request, response) => {
+app.post('/', (request, response) => {
 
-  var id = request.params.id
-  var body = _.pick(request.body, ['name'])
+    Subject(request.body).save().then((doc) => {
+        response.send(doc);
+    }, (error) => {
+        response.status(400).send(error);
+    })
+});
 
-//   Subject.find().then((id, {
-//       $set: request.body
-//   }, {
-//       new: true
-//   }).then((subject) => {
+app.patch('/unit', (request, response) => {
 
-//       if(!subject) {
-//           return response.status(404).send();
-//       }
+    //var id = request.params.id
+    var body = _.pick(request.body, ['name', 'subjectId'])
+    
+    if (!ObjectID.isValid(body.subjectId)) {
+        return response.status(404).send();
+    }
 
-//       response.send({subject})
-      
-//   }).catch((error) => {
-//       response.status(400).send(error)
-//   })
+    var unitJson = { "_id": new ObjectID(), "name": body.name, "chapters": [] };
+    
+    Subject.findOneAndUpdate(
+        { _id: body.subjectId }, 
+        { $push: { units: unitJson } },
+        { "new": true },
+        function (error, document) {
+            if (error) {
+                response.status(400).send(error)
+            } else {
+                response.send({document})
+            }
+        }
+    );
 })
 
+app.patch('/unit/chapter', (request, response) => {
 
-// app.patch('/unit', (request, response) => {
+    var body = _.pick(request.body, ['name', 'subjectId','unitId'])
+    
+    if (!ObjectID.isValid(body.subjectId) && !ObjectID.isValid(body.unitId)) {
+        return response.status(404).send();
+    }
 
-//     var id = request.params.id
-//     var body = _.pick(request.body, ['unitId',''])
-  
-//     //console.log(`subject patching ${body}`);
-  
-//     if (!ObjectID.isValid(id)) {
-//         return response.status(404).send();
-//     }
-  
-//     Subject.findByIdAndUpdate(id, {
-//         $set: request.body
-//     }, {
-//         new: true
-//     }).then((subject) => {
-  
-//         if(!subject) {
-//             return response.status(404).send();
-//         }
-  
-//         response.send({subject})
-        
-//     }).catch((error) => {
-//         response.status(400).send(error)
-//     })
-//   })
+    var chapterJson = { "_id": new ObjectID(), "name": body.name };
+    
+    Subject.update(
+        { "_id": ObjectID(body.subjectId), "units": { "$elemMatch": { "_id": ObjectID(body.unitId) } } },
+        { $push: { "units.$[t].chapters": chapterJson } },
+        { arrayFilters: [ { "t._id": ObjectID(body.unitId) }] },
+        function (error, document) {
+            if (error) {
+                response.status(400).send(error)
+            } else {
+                response.send(document)
+            }
+        }
+    );
+})
 
 module.exports = app;
